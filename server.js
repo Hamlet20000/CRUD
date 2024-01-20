e090dd3bf22cb7e7791af60a6e607ad2c56b49e1
Hamlet20000 <hammkrtchyan7@gmail.com>
2024-01-20 15:29:22 +0400

    change file

--- server.js
+++ server.js
@@ -1,23 +1,45 @@
-var express = require("express");
-var path = require("path");
-const bodyParser = require('body-parser');
-var app = express();
+// var express = require("express");
+// var path = require("path");
+// const bodyParser = require('body-parser');
+// var app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(express.static('public'));
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(express.static('public'));
 
-app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname,'./public/form.html'));
+// app.get("/", function(req, res){
+//     res.sendFile(path.join(__dirname,'./public/form.html'));
+// });
+
+// app.post('/addName', (req, res) => {
+//     const name = req.body.name;
+//     const age = req.body.age;
+//     console.log('Received datas:', name , age );
+//     res.redirect('/');
+//  });
+
+// app.listen(3000, function(){
+//    console.log("Example is running on port 3000");
+// });
+const mongoose = require('mongoose');
+
+const connectionString = 'mongodb+srv://hamletmkrtchyan1:hamlet2007@hamlet2007.jsr33uh.mongodb.net/?retryWrites=true&w=majority/sample_mflix';
+
+
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', async () => {
+	console.log('Connected to MongoDB!');
+
+	try {
+		const allMovies = await mongoose.connection.db.collection('movies').find().toArray(); // .insertMany(newMovies);
+
+		console.log('All Movies:', allMovies);
+	} catch (error) {
+		console.error('Error retrieving movies:', error);
+	} finally {
+		mongoose.connection.close();
+	}
 });
 
-app.post('/addName', (req, res) => {
-    const name = req.body.name;
-    const age = req.body.age;
-    console.log('Received datas:', name , age );
-    res.redirect('/');
- });
-
-app.listen(3000, function(){
-   console.log("Example is running on port 3000");
-});
